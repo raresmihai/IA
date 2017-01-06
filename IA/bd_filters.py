4d73edb8b00a8ce8a818a01866b3cb8be7b3d229
cypmaster14 <lila.lazar@info.uaic.ro>
2017-01-06 21:43:29 +0200

    Add math function call

--- IA/bd_filters.py
+++ IA/bd_filters.py
@@ -1,30 +1,38 @@
 from bd_aiml import respond_from_aiml
+from math_module import get_math_response
+from scraping import get_answer
 
 def get_yes_or_no_response(input):
-    #TO DO
+    # TO DO
     return ''
+
 
 def get_personal_question_response(input):
-    #TO DO
+    # TO DO
     return respond_from_aiml(input)
+
 
 def get_choose_between_response(input):
-    #TO DO
+    # TO DO
     return ''
+
 
 def get_difference_between_response(input):
-    #TO DO
+    # TO DO
     return ''
+
 
 def get_info_about_response(input):
-    #TO DO
+    # TO DO
     return ''
+
 
 def get_math_question_response(input):
-    #TO DO
-    return ''
+    # TO DO
+    #Pe ultima pozitie am nevoie sa fie fraza intreaga introduse sa utilizator
+    return get_math_response(input[len(input) - 1])
+
 
 def get_doubts_response(input):
-    #TO DO
+    # TO DO
     return respond_from_aiml(input)
-
